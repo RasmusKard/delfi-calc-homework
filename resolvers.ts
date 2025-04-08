a8e691855b3fd29432dda8df02087ee2ce29c983
RasmusKard <rasmuskard@gmail.com>
2025-04-08 17:45:14 +0300

    add: basic operations

--- resolvers.ts
+++ resolvers.ts
@@ -3,7 +3,16 @@ import { Resolvers } from "./src/generated/graphql.js";
 export const resolvers: Resolvers = {
   Query: {
     sum: (_, { input }) => {
-      return input.reduce((sum, partialSum) => sum + partialSum, 0);
+      return input.reduce((value, partialResult) => value + partialResult);
+    },
+    subtract: (_, { input }) => {
+      return input.reduce((value, partialResult) => value - partialResult);
+    },
+    multiply: (_, { input }) => {
+      return input.reduce((value, partialResult) => value * partialResult);
+    },
+    divide: (_, { input }) => {
+      return input.reduce((value, partialResult) => value / partialResult);
     },
   },
 };
